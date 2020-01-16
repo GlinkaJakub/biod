bc974023fc521b9acb5131591b5107e358a4734d
Jakub Glinka <glinka.jakub@outlook.com>
2020-01-16 14:27:56 +0100

    Initial commit

---
+++ src/main/java/com/glinka/biod/BiodApplication.java
@@ -0,0 +1,13 @@
+package com.glinka.biod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiodApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BiodApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/glinka/biod/BiodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glinka.biod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiodApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
